d8ec0add1bb01d4ad2a73b7d474d955f39ae34be
fbertra <fbertra@gmail.com>
2021-11-27 17:29:28 +0000

    union type, indentation

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -2,8 +2,17 @@ package example
 
 object Hello extends Greeting with App {
   println(greeting)
+
+  println (greet ("yo", "hola"))
+  println (greet ("me"))
 }
 
-trait Greeting {
+trait Greeting:
   lazy val greeting: String = "hello"
-}
+
+  def greet (to: String, msg: String | Null = null): String = 
+    val _msg = msg match 
+      case msg: String => msg
+      case null => greeting
+
+    s"${_msg} $to"
