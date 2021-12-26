1b92956c4ea6d3d928d9de9ceea24c3969ece78f
fbertra <fbertra@gmail.com>
2021-12-26 21:55:35 +0000

    everything is IO

--- src/main/scala/cl/fbd/Main.scala
+++ src/main/scala/cl/fbd/Main.scala
@@ -8,6 +8,7 @@ import cats.effect.unsafe.IORuntime
 import scala.concurrent.ExecutionContext.global
 
 import org.http4s.blaze.client.BlazeClientBuilder
+import org.http4s.client.Client
 
 import cl.fbd.domain.{PokemonSvc, PokemonData}
 import cl.fbd.json.FromJson._
@@ -19,31 +20,31 @@ object HttpClient extends IOApp.Simple {
   val pokemonApiURL = "https://pokeapi.co/api/v2/pokemon?limit=5"
   val pokemonAbilityURL = "https://pokeapi.co/api/v2/ability/"
 
-  def run = {
-    
+  def callPokemonAbility (client: Client [IO], data: PokemonData): IO [Unit] = {
+    val urlSpiitted = data.url.split ("/")
+
+    val id = urlSpiitted (urlSpiitted.length - 1)
+
+    val expectedAbility = client.expect[PokemonAblility](pokemonAbilityURL + id)
+
+    expectedAbility.flatMap (ability => IO (println (ability.toString ())))
+  }
+
+  def run = {    
     val builder = BlazeClientBuilder[IO](scala.concurrent.ExecutionContext.global)
     
     builder.resource.use { client => {
-        val expected = client.expect[PokemonSvc](pokemonApiURL)
+        val expectedSvc = client.expect[PokemonSvc](pokemonApiURL)
 
-        val ret = expected.flatMap (pokemonSvc => {
-            println (pokemonSvc.toString ())
+        expectedSvc.flatMap (pokemonSvc => {
+            val logSvc = IO (println (pokemonSvc.toString ()))
 
             val res = for (pokemonData <- pokemonSvc.results) yield {
-              val urlSpiitted = pokemonData.url.split ("/")
-
-              val id = urlSpiitted (urlSpiitted.length - 1)
-
-              val expectedAbility = client.expect[PokemonAblility](pokemonAbilityURL + id)
-
-              expectedAbility.map (ability => println (ability.toString ()))
+              callPokemonAbility (client, pokemonData)
             }
 
-            res.sequence.map(_ => ())
-            // pokemonSvc
+            logSvc *> res.sequence.map(_ => ())
           })
-
-        ret
       }
     }
   }
